ff85cc52ef894ebb232f7bfa4c523a0f8e8432c7
Claudio Mignanti <c.mignanti@unidata.it>
2015-07-21 10:36:21 +0200

    docker: fix SENTRY config

--- docker/wsgi.py
+++ docker/wsgi.py
@@ -1,17 +1,23 @@
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+"""
+WSGI config for nodeshot project.
 
-from django.conf import settings
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
 
-# sentry WSGI handler if configured
-# more info here: http://raven.readthedocs.org/en/latest/config/django.html#wsgi-middleware
-if settings.SENTRY_ENABLED:
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    from django.core.handlers.wsgi import WSGIHandler
-    application = Sentry(WSGIHandler())
-# django default wsgi
-else:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
+"""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from nodeshot.conf.wsgi import application
+
